7e0ee57e51df53640597bbd3bfd54e0bc28f119e
VNC <chrhodes@cox.net>
2022-12-08 17:35:15 -0800

    Using System.CommandLine

--- Docker/ConsoleApps/ConsoleApp2/Program.cs
+++ Docker/ConsoleApps/ConsoleApp2/Program.cs
@@ -1,11 +1,60 @@
-﻿namespace ConsoleApp2
+﻿using System;
+using System.CommandLine;
+using System.Xml;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello, World from ConsoleApp2 !");
-            Console.ReadLine();
+            var rootCommand = new RootCommand("Sample app for learning Docker");
+
+            var loop = true;
+
+            rootCommand.SetHandler(() =>
+            {
+                Console.WriteLine("Enter two numbers.  Any non-numeric value exits");
+
+                while (loop)
+                {
+                    int number1;
+                    int number2;
+
+                    Console.Write("Enter First Number: ");
+                    var input1 = Console.ReadLine();
+
+                    if (!int.TryParse(input1, out number1) )
+                    {
+                        Console.Error.WriteLine($"Cannot parse {input1} as number");
+                        loop = false;
+                    }
+                    else
+                    {
+                        Console.Write("Enter Second Number: ");
+                        var input2 = Console.ReadLine();
+
+                        if (!int.TryParse(input2, out number2))
+                        {
+                            Console.Error.WriteLine($"Cannot parse {input2} as number");
+                            loop = false;
+                        }
+                        else
+                        {
+                            var result = number1 + number2;
+
+                            Console.WriteLine($"{number1} + {number2} = {result}");
+                        }
+
+                        if (!loop)
+                        {
+                            Console.WriteLine("Exiting");
+                        }
+                    }
+                }
+            });
+
+            return await rootCommand.InvokeAsync(args);
         }
     }
 }
